58e5cddba4c3e35fd3001dcd211d08a7d5d515ed
umapraveen <umapraveen2008@yahoo.com>
2021-12-14 00:19:30 +0530

    Initial Commit

---
+++ Shared/codingchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  codingchallengeApp.swift
+//  Shared
+//
+//  Created by Asfiya Tabassum on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct codingchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
